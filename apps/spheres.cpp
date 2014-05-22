ce67dd20cc7e868bd30207c91b43e62322398965
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2014-05-22 11:35:36 +0200

    First functional volume generation

--- apps/spheres.cpp
+++ apps/spheres.cpp
@@ -13,14 +13,25 @@
 
 namespace po = boost::program_options;
 
+typedef unsigned char               PixelType;
+typedef itk::Image< PixelType, 3 >  ImageType;
+
 int main( const int argc, const char* argv[] )
 {
+    // Default values
+    size_t size = 32;
+    std::string outputFile( "volume.mhd" );
+
     // Argument parsing
     po::variables_map vm;
-    po::options_description desc( "Supported options:" );
+    po::options_description desc( "Supported options" );
     desc.add_options()
         ( "help, h", "show help message." )
-        ( "version, v", "Show program name and version." );
+        ( "version, v", "Show program name and version." )
+        ( "size,s", po::value< size_t >()->default_value( size ),
+          "Size of the output volume" )
+        ( "output,o", po::value< std::string >()->default_value( outputFile ),
+          "Name of the output volume file" );
     po::store( parse_command_line( argc, argv, desc ), vm );
     po::notify( vm );
 
@@ -32,38 +43,40 @@ int main( const int argc, const char* argv[] )
 
     if( vm.count( "version" ))
     {
-        std::cout << "HelloRunner version " << fivox::Version::getString()
+        std::cout << argv[0] << " version " << fivox::Version::getString()
                   << std::endl
                   << "Copyright (c) BBP/EPFL 2014." << std::endl;
         return EXIT_SUCCESS;
     }
 
-    const unsigned int Dimension = 2;
+    if( vm.count( "size" ))
+        size = vm["size"].as< size_t >();
+    if( vm.count( "output" ))
+        outputFile = vm["output"].as< std::string >();
 
-    const char * inputFileName = argv[1];
-    const char * outputFileName = argv[2];
-    const int radiusValue = atoi( argv[3] );
+    ImageType::SizeType vSize;
+    vSize.Fill( size );
 
-    typedef unsigned char                       PixelType;
-    typedef itk::Image< PixelType, Dimension >  ImageType;
+    ImageType::RegionType region;
+    region.SetSize( vSize );
 
-    typedef itk::ImageFileReader< ImageType > ReaderType;
-    ReaderType::Pointer reader = ReaderType::New();
-    reader->SetFileName( inputFileName );
+    ImageType::Pointer volume = ImageType::New();
+    volume->SetRegions( region );
+    volume->Allocate();
 
     typedef itk::MeanImageFilter< ImageType, ImageType >  FilterType;
     FilterType::Pointer meanFilter = FilterType::New();
 
     FilterType::InputSizeType radius;
-    radius.Fill( radiusValue );
+    radius.Fill( 5.f );
 
     meanFilter->SetRadius( radius );
-    meanFilter->SetInput( reader->GetOutput() );
+    meanFilter->SetInput( volume );
 
     typedef itk::ImageFileWriter< ImageType > WriterType;
     WriterType::Pointer writer = WriterType::New();
     writer->SetInput( meanFilter->GetOutput() );
-    writer->SetFileName( outputFileName );
+    writer->SetFileName( outputFile );
 
     try
     {
