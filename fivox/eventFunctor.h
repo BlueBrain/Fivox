d230b96fcd9c7196a859b264cc6bd21c8bfb9239
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2014-06-19 10:31:50 +0200

    Minor tweaks

--- fivox/eventFunctor.h
+++ fivox/eventFunctor.h
@@ -46,7 +46,7 @@ public:
     TAccumulator sum = 0;
     static const float rho = 3.54f; //omh*m == 354 ohm*cm
     static const float factor = rho /( 4.f * M_PI );
-    static const float threshold2 = 100.f;
+    static const float threshold2 = 10.f;
 
     Vector3f base;
     const size_t components = std::min( point.Size(), 3u );
