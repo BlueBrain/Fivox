834f878c1be783f4d8145269307a281e20e95bde
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2014-07-02 20:14:11 +0200

    Declare transitive ITK dependency

--- CMake/Fivox.cmake
+++ CMake/Fivox.cmake
@@ -1,5 +1,5 @@
 
-set(FIVOX_PACKAGE_VERSION 1.0)
+set(FIVOX_PACKAGE_VERSION 0.1)
 set(FIVOX_REPO_URL ssh://bbpgit.epfl.ch/user/eilemann/Fivox.git)
 set(FIVOX_DEPENDS BBPSDK BBPTestData REQUIRED ITK Boost vmmlib)
 #see ubuntu bug 1328924: set(FIVOX_DEB_DEPENDS libinsighttoolkit4-dev)
