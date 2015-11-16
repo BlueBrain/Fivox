c4187a60a9c1694cd1c416d056d3e72217900ff9
Juan Hernando <jhernando@fi.upm.es>
2015-11-16 17:44:49 +0100

    Brion-only compartment report loader.
    Change-Id: I0b3281011a880a84b4964c0c7290c3515ac4b3f9

--- fivox/compartmentLoader.cpp
+++ fivox/compartmentLoader.cpp
@@ -9,7 +9,9 @@
 #include "event.h"
 #include "uriHandler.h"
 
-#include <BBP/BBP.h>
+#include <brion/brion.h>
+#include <brain/circuit.h>
+#include <brain/morphology.h>
 
 namespace fivox
 {
@@ -34,69 +36,73 @@ class CompartmentLoader::Impl
 public:
     Impl( fivox::EventSource& output, const URIHandler& params )
         : _output( output )
-        , _experiment( params.getConfig( ))
-        , _reader( *_experiment.reports().find( params.getReport( )),
-                   _experiment.cell_target(
-                       params.getTarget( _experiment.circuit_target( ))))
+        , _config( params.getConfig( ))
+        , _target( _config.parseTarget( params.getTarget( )))
+        , _report( _config.getReportSource( params.getReport( )),
+                   brion::MODE_READ, _target)
     {
-        const bbp::Cell_Target& target_ = _reader.getCellTarget();
-        bbp::Microcircuit& microcircuit = _experiment.microcircuit();
-        microcircuit.load( target_, bbp::NEURONS | bbp::MORPHOLOGIES );
-        _reader.updateMapping( target_ );
+        _report.updateMapping( _target );
 
-        size_t i = 0;
-        for( const uint32_t gid : target_ )
+        brain::Circuit circuit( _config );
+        const auto morphologies = circuit.loadMorphologies(
+            _target, brain::Circuit::COORDINATES_GLOBAL );
+
+        for( size_t i = 0; i != morphologies.size(); ++i )
         {
-            const bbp::Neuron& neuron = microcircuit.neuron( gid );
+            const brain::Morphology& morphology = *morphologies[i];
+
             // The voltage reports typically include the soma, dendrites and
             // first two axon sections. Here we will ignore the axon sections.
-            const bbp::Sections& sections = neuron.dendrites();
-
-            output.add( Event( neuron.position( ), 0.f ));
-            _sections.push_back(
-                SectionInfo( 1, _reader.getOffsets()[i][neuron.soma().id()] ));
-
-            for( const bbp::Section& section : sections )
+            const auto sections =
+                morphology.getSectionIDs({ brion::SECTION_SOMA,
+                                           brion::SECTION_DENDRITE,
+                                           brion::SECTION_APICAL_DENDRITE });
+            for( auto section : sections )
             {
-                const uint32_t id = section.id();
-                const size_t nCompartments =
-                    _reader.getCompartmentCounts()[i][id];
-                assert( nCompartments );
-                if( !nCompartments )
-                    return;
-                const float length = 1.f / float( nCompartments );
+                const auto& counts = _report.getCompartmentCounts()[i];
+                if( section >= counts.size( ))
+                    continue;
+                const size_t compartments = counts[section];
+                if( !compartments )
+                    continue; // This occurs in unreported axonal sections.
 
+                brion::floats samples;
+                samples.reserve( compartments );
+                const float length = 1.f / float( compartments );
                 for( float k = length * .5f; k < 1.0; k += length )
-                    output.add(
-                        Event( section.cross_section( k ).center(), 0.f ));
+                    samples.push_back( k );
 
-                _sections.push_back( SectionInfo( nCompartments,
-                                                  _reader.getOffsets()[i][id] ));
+                const auto points =
+                    morphology.getSectionSamples( section, samples );
+                for( const auto& point : points )
+                    output.add( Event( point.get_sub_vector< 3 >(), 0.f ));
+
+                _sections.push_back(
+                    SectionInfo( compartments,
+                                 _report.getOffsets()[i][section] ));
+
             }
-            ++i;
         }
     }
 
     bool load( const float time )
     {
-        bbp::CompartmentReportFrame frame;
-        if( !_reader.loadFrame( time, frame ))
+        const brion::floatsPtr values = _report.loadFrame( time );
+        if( !values )
         {
             LBERROR << "Could not load frame at " << time << "ms" << std::endl;
             return false;
         }
 
-        const bbp::floatsPtr& voltages = frame.getData< bbp::floatsPtr >();
         size_t index = 0;
-        for( size_t i = 0; i < _sections.size(); ++i )
+        for( const auto& section : _sections )
         {
-            const SectionInfo& info = _sections[i];
-            const uint64_t end = info.numCompartments + info.offset;
-
-            for( uint64_t offset = info.offset; offset < end; ++offset, ++index)
+            const uint64_t end = section.numCompartments + section.offset;
+            for( uint64_t offset = section.offset; offset < end;
+                 ++offset, ++index )
             {
-                _output.update( index,
-                             ( *voltages )[ offset ] - brion::MINIMUM_VOLTAGE );
+                _output.update(
+                    index, (( *values )[ offset ] - brion::MINIMUM_VOLTAGE ));
             }
         }
         LBINFO << "Updated " << index << " events at " << time << "ms"
@@ -104,17 +110,13 @@ public:
         return true;
     }
 
-    Vector2ui getFrameRange()
-    {
-        const float reportTime = _reader.getEndTime() - _reader.getStartTime();
-        const uint32_t numFrames = std::ceil( reportTime / _output.getDt( ));
-        return Vector2ui( 0, numFrames );
-    }
-
     fivox::EventSource& _output;
-    bbp::Experiment _experiment;
-    bbp::CompartmentReportReader _reader;
+
+    brion::BlueConfig _config;
+    brion::GIDSet _target;
     SectionInfos _sections;
+
+    brion::CompartmentReport _report;
 };
 
 CompartmentLoader::CompartmentLoader( const URIHandler& params )
@@ -122,7 +124,7 @@ CompartmentLoader::CompartmentLoader( const URIHandler& params )
     , _impl( new CompartmentLoader::Impl( *this, params ))
 {
     if( getDt() < 0.f )
-        setDt( _impl->_reader.getTimestep( ));
+        setDt( _impl->_report.getTimestep( ));
 }
 
 CompartmentLoader::~CompartmentLoader()
@@ -130,8 +132,8 @@ CompartmentLoader::~CompartmentLoader()
 
 Vector2f CompartmentLoader::_getTimeRange() const
 {
-    return Vector2f( _impl->_reader.getStartTime(),
-                     _impl->_reader.getEndTime( ));
+    return Vector2f( _impl->_report.getStartTime(),
+                     _impl->_report.getEndTime( ));
 }
 
 bool CompartmentLoader::_load( const float time )
