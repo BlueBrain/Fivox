098702d7c5d4f4026429915c354299702af2fc1e
Jafet Villafranca <jafet.villafrancadiaz@epfl.ch>
2016-10-21 10:21:41 +0200

    CR3 #65

--- apps/computeVSD/computeVSD.cpp
+++ apps/computeVSD/computeVSD.cpp
@@ -215,38 +215,34 @@ public:
             const auto& fileName = _vm["soma-pixels"].as< std::string >();
             std::ofstream file( fileName );
             if( !file.is_open( ))
-            {
                 LBERROR << "File " << fileName << " could not be opened"
                         << std::endl;
-                break;
-            }
-            file << "# Soma position and corresponding pixel index for "
-                    "each cell, in the following format:\n"
-                 << "#     gid [ posX posY posZ ]: i j\n"
-                 << "# File version: 1\n"
-                 << "# Fivox version: " << fivox::Version::getString()
-                 << std::endl;
-
-            if( file.bad( ))
-                break;
-
-            size_t i = 0;
-            const auto& gids = vsdLoader->getGIDs();
-            for( const auto& gid : gids )
+            else
             {
-                const auto pos = somaPositions[i++];
-                file << gid << " " << pos << ": "
-                     << std::floor(( pos[0] - origin[0] ) / spacing[0] ) << " "
-                     << std::floor(( pos[2] - origin[2] ) / spacing[1] )
+                file << "# Soma position and corresponding pixel index for "
+                        "each cell, in the following format:\n"
+                     << "#     gid [ posX posY posZ ]: i j\n"
+                     << "# File version: 1\n"
+                     << "# Fivox version: " << fivox::Version::getString()
                      << std::endl;
-                if( file.bad( ))
-                    break;
-            }
 
+                size_t i = 0;
+                const auto& gids = vsdLoader->getGIDs();
+                for( const auto& gid : gids )
+                {
+                    if( file.bad( ))
+                        break;
+                    const auto& pos = somaPositions[i++];
+                    file << gid << " " << pos << ": "
+                         << std::floor((pos[0] - origin[0]) / spacing[0]) << " "
+                         << std::floor((pos[2] - origin[2]) / spacing[1])
+                         << std::endl;
+                }
+            }
             if( file.good( ))
                 LBINFO << "Soma positions written as " << fileName << std::endl;
             else
-                LBERROR << "Could not write to " << fileName << std::endl;
+                LBERROR << "Error while writing to " << fileName << std::endl;
         }
 
         output->SetSpacing( spacing );
